71b3892491168a9ce56bf24380f66c1c5b809509
DAVID HOUSHANGI <davidhoushangi@yahoo.com>
2025-11-30 16:40:09 +0300

    Update train_test_split.py

--- RandomForestRegressor/train_test_split.py
+++ RandomForestRegressor/train_test_split.py
@@ -1,1 +1,12 @@
+# Training test split
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+print("Train size:", X_train.shape[0])
+print("Test size:", X_test.shape[0])
 
