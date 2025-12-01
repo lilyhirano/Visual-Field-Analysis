ec1da3f3c445f482d49506d3ae0bae93484d09ae
DAVID HOUSHANGI <davidhoushangi@yahoo.com>
2025-12-01 20:24:25 +0300

    Update progression_model.py

--- glaucoma-progression-from-vf/progression_model.py
+++ glaucoma-progression-from-vf/progression_model.py
@@ -13,7 +13,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
-# cleaned CSV produced by data_cleaning.py
+# cleaned data:
+
 RAW_PATH = "data/uw_vf_clean.csv"
 
 
