6119ba5e72696a03461b0bdbd94bb792629e043d
DAVID HOUSHANGI <davidhoushangi@yahoo.com>
2025-12-01 17:04:30 +0300

    Create data_cleaning.py

---
+++ glaucoma-progression-from-vf/data_cleaning.py
@@ -0,0 +1,14 @@
+# data_cleaning.py
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# make plots look a bit nicer
+sns.set(style="whitegrid", context="talk")
+
+# path to the UW VF dataset
+# Dataset:
+DATA_PATH = "data/uw_vf.csv" 
+
